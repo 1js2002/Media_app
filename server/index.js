60bfc11a3bec45f26f5ca7fc77e3e94e769f5042
dinesh <dgoud99120@gmail.com>
2023-07-27 13:40:08 +0530

    auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 
@@ -38,6 +39,9 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 //Mongoose Setup
 const PORT = process.env.PORT || 3000;
